860c549ca9698cb7ad11ce7e96e301bdfb81b88f
sanjay kumar chaurasia <sanjaykumarchaurasia@sanjays-Mac-mini.local>
2018-12-01 12:10:21 +0530

    first commit

---
+++ app/src/main/java/com/example/sanjaykumarchaurasia/demo2/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.sanjaykumarchaurasia.demo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        String info = intent.getExtras().getString("name");
+        Toast.makeText(this, "" +
+                "" +
+                info, Toast.LENGTH_SHORT).show();
+
+
+
+       // TextView txtView = (TextView) findViewById(R.id.textView2);
+       // txtView.setText(info);
+        setContentView(R.layout.activity_second);
+    }
+}
